a285588205ecd82fcc00065f91308a7574901f6f
Mohamed Hamouda <107848993+mohamedhamoudadevop@users.noreply.github.com>
2023-04-14 14:57:49 +0200

    done change to red color

--- lib/tabs/ahadeth.dart
+++ lib/tabs/ahadeth.dart
@@ -6,7 +6,7 @@ class AhadethTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.cyanAccent,
+      color: Colors.red,
     );
   }
 }
